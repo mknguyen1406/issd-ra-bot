985f6638c0bed5a28788706a2f3afb176bdbab32
Minh-Kha <mk.nguyen1406@gmail.com>
2019-07-16 16:45:48 +0200

    further information with result log

--- bots/dispatchBot.js
+++ bots/dispatchBot.js
@@ -70,7 +70,7 @@ class DispatchBot extends ActivityHandler {
         async function writeToFile(jsonData, context) {
             const filename = jsonData.userId;
             await context.sendActivity("Result received\n Filename is: " + filename); 
-            await context.sendActivity("Name of directory is: " + __dirname); 
+            await context.sendActivity("Name of directory is: " + require.main.filename); 
             fs.writeFile(filename + ".txt", jsonData, async function(err) {
                 if (err) {
                     await context.sendActivity("Error ocurred while saving the result. \n" + filename);
