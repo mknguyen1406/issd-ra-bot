8939ce14a891666f3b9e83ab5a743aa35d181691
Minh-Kha Nguyen <mk.nguyen1406@gmail.com>
2019-10-06 18:26:10 +0200

    getDate bug fixed

--- public/functions.js
+++ public/functions.js
@@ -1014,8 +1014,8 @@ function dispatchBotEvent(chatbotResponse, type, turnContext, callback) {
 
 function getDate() {
     const date = new Date();
-    const day = date.getDay() < 10 ? "0" + date.getDay() : date.getDay();
-    const month = date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth();
+    const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    const month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
     const year = date.getFullYear();
     const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
     const min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
